466cbe2b9f5cfa461a18a2f3ab53cad85344498a
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2022-04-02 14:48:16 +0200

    Add global font definitions

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -3,3 +3,9 @@ push = require 'lib/push'
 Timer = require 'lib/knife.timer'
 
 require 'src/constants'
+
+gFonts = {
+    ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+    ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+    ['large'] = love.graphics.newFont('fonts/font.ttf', 32)
+}

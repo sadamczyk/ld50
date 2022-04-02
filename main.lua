8928021769a65ff4499c7a83319a73bda1b4ba1c
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2022-04-02 19:22:26 +0200

    Add bomb tweening from enemy towards player

--- main.lua
+++ main.lua
@@ -15,20 +15,53 @@ function love.load()
     -- Create entities
     local player = {
         position = { x = VIRTUAL_WIDTH / 2, y = VIRTUAL_HEIGHT / 2 },
-        drawable = true,
-        damagable = { currHP = 3, maxHP = 3 }
+        drawable = { radius = 10 },
+        damagable = { currHP = 3, maxHP = 3 },
+        storage = { max = 2, curr = 0 }
     }
     
     local enemy = {
-        position = { x = VIRTUAL_WIDTH / 2 + 40, y = VIRTUAL_HEIGHT / 2 },
-        drawable = true,
-        damagable = { currHP = 3, maxHP = 3 }
+        position = { x = VIRTUAL_WIDTH / 2 + 100, y = VIRTUAL_HEIGHT / 2 },
+        drawable = { radius = 10 },
+        damagable = { currHP = 3, maxHP = 3 },
+        storage = { max = 10, curr = 10 },
+        AI = true
     }
 
     entities = {
         player,
         enemy
     }
+
+    -- Start timers for throwing bombs
+    Timer.every(5, function ()
+        -- Create a new bomb entity
+        local bomb = {
+            position = { x = enemy.position.x, y = enemy.position.y },
+            drawable = { radius = 2 },
+            removable = false
+        }
+
+        table.insert(entities, #entities, bomb)
+
+        -- Tween that new bomb to the player
+        Timer.tween(5,  {
+            [bomb.position] = { x = player.position.x, y = player.position.y }
+        })
+        :finish(function ()
+            bomb.removable = true
+        end)
+    end)
+
+    -- Removables
+    removeEntities = System(
+        { 'removable' },
+        function (rem, entities, i)
+            if rem then
+                table.remove(entities, i)
+            end
+        end
+    )
 end
 
 function love.resize(w, h)
@@ -36,7 +69,11 @@ function love.resize(w, h)
 end
 
 function love.update(dt)
-    -- Timer.update(dt)
+    for i = #entities, 1, -1 do
+        removeEntities(entities[i], entities, i)
+    end
+
+    Timer.update(dt)
 end
 
 function love.draw()
@@ -47,11 +84,14 @@ function love.draw()
     love.graphics.rectangle('fill', 0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT)
     love.graphics.setColor(0, 0, 0, 1) -- reset color
 
+    love.graphics.setFont(gFonts['small'])
+    love.graphics.print(#entities, 20, 20)
+
     -- Draw all drawables
     local drawDrawables = System(
-        { 'position', '-drawable' },
-        function (pos)
-            love.graphics.circle('fill', pos.x, pos.y, 5)
+        { 'position', 'drawable' },
+        function (pos, drawable)
+            love.graphics.circle('fill', pos.x, pos.y, drawable.radius)
         end
     )
 
